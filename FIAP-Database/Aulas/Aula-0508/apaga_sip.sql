3c07faf58652eebd80ef98e2302b66614457729d
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2020-08-05 22:38:47 -0300

    Class 05/08 - Database
    Oracle database, SQL Datamodeler

---
+++ FIAP-Database/Aulas/Aula-0508/apaga_sip.sql
@@ -0,0 +1,7 @@
+-- Gerado por Oracle SQL Developer Data Modeler 19.4.0.350.1424
+--   em:        2020-07-31 10:34:28 BRT
+--   site:      Oracle Database 11g
+--   tipo:      Oracle Database 11g
+DROP TABLE t_sip_departamento CASCADE CONSTRAINTS;
+
+DROP TABLE t_sip_funcionario CASCADE CONSTRAINTS;
