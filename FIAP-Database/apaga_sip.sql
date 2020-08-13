7f612d95e22918f5f40b2e80ea39a7549182c749
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2020-08-12 23:43:25 -0300

    Constraints e herança

---
+++ FIAP-Database/apaga_sip.sql
@@ -0,0 +1,29 @@
+-- Gerado por Oracle SQL Developer Data Modeler 19.4.0.350.1424
+--   em:        2020-08-12 23:29:39 BRT
+--   site:      Oracle Database 11g
+--   tipo:      Oracle Database 11g
+
+
+
+DROP TABLE t_pes_fisica CASCADE CONSTRAINTS;
+
+DROP TABLE t_pes_juridica CASCADE CONSTRAINTS;
+
+DROP TABLE t_wedo_cli_end CASCADE CONSTRAINTS;
+
+DROP TABLE t_wedo_cliente CASCADE CONSTRAINTS;
+
+DROP TABLE t_wedo_endereco_correio CASCADE CONSTRAINTS;
+
+DROP TABLE t_wedo_entrega_semana CASCADE CONSTRAINTS;
+
+DROP TABLE t_wedo_img_produt0 CASCADE CONSTRAINTS;
+
+DROP TABLE t_wedo_item_lista_desejo CASCADE CONSTRAINTS;
+
+DROP TABLE t_wedo_lista_desejo CASCADE CONSTRAINTS;
+
+DROP TABLE t_wedo_produto CASCADE CONSTRAINTS;
+
+DROP TABLE t_wedo_tel_cliente CASCADE CONSTRAINTS;
+
