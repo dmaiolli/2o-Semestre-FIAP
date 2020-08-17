56b243be32fd2a473346a347e0da8445642add24
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2020-08-16 22:53:09 -0300

    exercises 6 to 10 done

--- FIAP-Javascript/Exercicios/js-basics/6-10/exs.js
+++ FIAP-Javascript/Exercicios/js-basics/6-10/exs.js
@@ -32,3 +32,83 @@ function MBytesToBytes(Mbytes){
 }
 
 MBytesToBytes(200)
+
+
+// ex8
+function suspeito(respostasPGT){
+    respPositivas = 0
+    for(pergunta of respostasPGT){
+        if(pergunta === 'sim'){
+            respPositivas++;
+        }
+    }
+
+    if(respPositivas === 5){
+        console.log("Assassino")
+    } else if(respPositivas >= 3){
+        console.log("Cúmplice");
+    } else if(respPositivas === 2){
+        console.log("Suspeita");
+    } else{
+        console.log("Inocente");
+        
+    }
+}
+
+respostas = ["sim", 'sim', "sim", "sim", 'sim']
+suspeito(respostas)
+
+
+// ex9
+function mediaAluno(notas){
+    soma = 0;
+    for(nota of notas){
+        soma += nota;
+    }
+
+    media = soma / notas.length;
+    aprovado = false;
+    if(media > 9){
+        aprovado = true;
+        console.log(`Conceito: A, Aprovado? ${aprovado}`);
+    }
+    else if(media > 7.5){
+        aprovado = true;
+        console.log(`Conceito: A, Aprovado? ${aprovado}`);
+    }
+    else if(media > 6){
+        aprovado = true;
+        console.log(`Conceito: A, Aprovado? ${aprovado}`);
+    }
+    else if(media > 4){
+        console.log("D");
+    }
+    else{
+        console.log("E");
+    }
+    
+}
+
+notas = [10,10,10,10]
+mediaAluno(notas)
+
+
+// ex10
+function totalHotel(diarias){
+    const diaria = 360
+    if(diarias > 15){
+        taxaServico = diarias * 35.50
+    }
+    else if(diarias === 15){
+        taxaServico = diarias * 46
+    }
+    else if(diarias < 15){
+        taxaServico = diarias * 58
+    }
+
+    total = taxaServico + diarias * 360
+    console.log(`Total taxa de serviço: ${taxaServico}\nTotal de dias: ${diarias}\nTotal: ${total}`);
+    
+}
+
+totalHotel(15)
